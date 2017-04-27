f881310f6b3ca47a8be754b0f7e4b40eab995bd7
Nurudeen Ibrahim <ebroyeem90@gmail.com>
2017-04-28 00:47:10 +0100

    Update the test file;

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,44 +1,49 @@
-/* i intend to create an instance of invertedIndex class called "index"
- *which would have access to fileContent property,createIndex method,
- *getIndex method, and searchIndex method.
- */
+
+const index = require('../src/inverted-index.js');
+
+
+index.createIndex('book');
 
 describe('Read book data', () => {
-  it('should check if the file content is a valid JSON array', () => {
+  it('should return true for valid JSON array', () => {
     expect(Array.isArray(index.fileContent)).toBe(true);
     expect(index.fileContent[0] instanceof Object).toBe(true);
-    expect(index.fileContent[index.fileContent.length] instanceof Object).toBe(true);
+    expect(index.fileContent[index.fileContent.length - 1] instanceof Object).toBe(true);
   });
 
-  it('should check if the JSON array is not empty', () => {
+  it('should not return zero for file content length', () => {
     expect(index.fileContent.length).not.toBe(0);
   });
 });
 
 describe('Populate index', () => {
-  it('should check if the index created is correct', () => {
-    expect(index.getIndex('life')).toEqual([1]);
-    expect(index.getIndex('tomorrow')).toEqual([0]);
+  it('should return [0, 1] for "the" in the "book" document', () => {
+    expect(index.index.book.the).toEqual([0, 1]);
+  });
+
+  it('should return [0] for "inquiry" in the "book" document', () => {
+    expect(index.index.book.inquiry).toEqual([0]);
   });
 });
 
 describe('Search index', () => {
-  it('should check if search result is correct', () => {
-    expect(index.searchIndex('life')).toEqual(['life', [1]]);
-    expect(index.searchIndex('tomorrow')).toEqual(['tomorrow', [0]]);   
+  it('should return { "third": [1] } for the word "third"', () => {
+    expect(index.searchIndex('third')).toEqual({ 'third': [1] });
   });
 
-  it('should check if an array of search terms is handled', () => {
-    expect(index.searchIndex(['life', 'tomorrow'])).toEqual([['life', [1]], ['tomorrow', [0]]]);
+  it('should return { "set": [0, 1] } for the word "Set"', () => {
+    expect(index.searchIndex('Set')).toEqual({ 'set': [0, 1] });
   });
 
-  it('should check if varied number of search arguments is handled', () => {
-    expect(index.searchIndex('life', 'tomorrow')).toEqual([['life', [1]], ['tomorrow', [0]]]);
-    expect(index.searchIndex('life', 'tomorrow', 'is')).toEqual([['life', [1]], ['tomorrow', [0]], ['is', [0, 1]]]);
+  it('should return { "understand":[0,1], "from": [1] } for array ["understand","from"]', () => {
+    expect(index.searchIndex(['understand', 'from'])).toEqual({ 'understand':[0,1], 'from': [1] });
   });
 
-  it('should check for words in both documents', () => {
-    expect(index.searchIndex('is')).toEqual([['is', [0, 1]]]);
-    expect(index.searchIndex('life', 'is', '')).toEqual([['life', [1]], ['is', [0, 1]], ['tomorrow', [1]]]);
+  it('should return toEqual({ "to": [0, 1], "is": [1], "into": [0] }) for "to","is","beginner"', () => {
+    expect(index.searchIndex('to', 'is', 'into')).toEqual({ 'to': [0, 1], 'is': [1], 'into': [0] });
+  });
+
+  it('should return [-1] for any word not present', () => {
+    expect(index.searchIndex('catwalk')).toEqual({ 'catwalk': [-1] });
   });
 });
