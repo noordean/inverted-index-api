871b2eee6114fa62f8c5f1bdc194d98f4825b464
Nurudeen Ibrahim <ebroyeem90@gmail.com>
2017-04-28 18:32:29 +0100

    Implement /create and /search endpoints on the server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 
-const app = express();
+const bodyParser = require('body-parser');
 
-app.listen(process.env.PORT,function(){
-	console.log("server now running at "+process.env.PORT)
+const index = require(__dirname+'/src/inverted-index.js');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+	console.log('server now running at ' + process.env.PORT);
+});
+
+app.post('/api/create', (req, res) => {
+  index.createIndex(req.body.file);
+  res.json({ message: 'Index created' });
+});
+
+app.post('/api/search', (req, res) => {
+  res.json(index.searchIndex(req.body.token));
 });
