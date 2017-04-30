d7d6b1178f50c9c34e806e786208b35747dfd448
Nurudeen Ibrahim <ebroyeem90@gmail.com>
2017-04-30 22:28:03 +0100

    Integrate supertest

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const index = require(__dirname+'/src/inverted-index.js');
+const supertest = require('supertest');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,3 +19,27 @@ app.post('/api/create', (req, res) => {
 app.post('/api/search', (req, res) => {
   res.json(index.searchIndex(req.body.searchTerm));
 });
+
+supertest(app)
+        .post('/api/create')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .send({ fileName: 'invalidBook.json'})
+        .expect({ error: 'Index could not be created, invalid JSON file selected'})
+        .end((err, res) => {
+          if(err){
+            throw err;
+          }
+        });
+
+supertest(app)
+        .post('/api/search')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .send({ searchTerm: 'the'})
+        .expect({error: 'Index has not been created. Kindly create index before searching'})
+        .end((err, res) => {
+          if (err){
+            throw err;
+          }
+        });
