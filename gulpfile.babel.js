bfed15af6e9ac83b0d92fda8c800866514fc4ed8
Nurudeen Ibrahim <ebroyeem90@gmail.com>
2017-05-04 14:41:32 +0100

    Fix babel totally

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,56 +1,45 @@
-// const gulp = require('gulp');
 
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import jasmine from 'gulp-jasmine';
-
-// const jasmine = require('gulp-jasmine');
-const nodemon = require('gulp-nodemon');
-const istanbul = require('gulp-istanbul');
-const coveralls = require('gulp-coveralls');
-// const babel = require('gulp-babel');
+import path from 'path';
+import nodemon from 'gulp-nodemon';
+import istanbul from 'gulp-istanbul';
+import coveralls from 'gulp-coveralls';
 
 
-gulp.task('run-tests', () => {
-  gulp.src('tests/inverted-index-test.js')
+
+// this transpiles all .js files except those in dist and node_modules folders
+gulp.task('babel', () => {
+  return gulp.src(['./**/*.js', '!dist/**', '!node_modules/**'])
   .pipe(babel())
+  .pipe(gulp.dest('dist'));
+});
+
+// this runs the jasmine tests through an already transpiled file
+gulp.task('run-tests', ['babel'], () => {
+  gulp.src(path.join('dist', 'tests', 'inverted-index-test.js'))
   .pipe(jasmine());
 });
 
-gulp.task('serve', () => {
+// this starts the server at the specified port in .env file
+gulp.task('serve', ['babel'], () => {
   nodemon({
-    script: 'server.js',
+    script: path.join('dist', 'server.js'),
     ext: 'js',
     env: { 'NODE_ENV': 'development' }
   });
 });
 
-/*gulp.task('coverage', () => {
-    return gulp.src('src/inverted-index.js')
-      .pipe(istanbul({includeUntested: true}))
-      .on('finish', () => {
-        gulp.src('tests/inverted-index-test.js')
-          .pipe(jasmine())
-          .pipe(istanbul.writeReports({
-            dir: 'coverage-report',
-            reporters: ['lcov'],
-            reportOpts: { dir: 'coverage-report'}
-          }));
-      });
-});*/
-
-
-// for coverage task
+// the coverage task depends on 'pre-test' and 'test' to give coverage report
 gulp.task('pre-test', () => {
-  return gulp.src(['src/inverted-index.js'])
-    .pipe(babel())
+  return gulp.src([path.join('dist', 'src', 'inverted-index.js')])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], () => {
-  return gulp.src(['tests/inverted-index-test.js'])
-    .pipe(babel())
+  return gulp.src([path.join('dist', 'tests', 'inverted-index-test.js')])
     .pipe(jasmine())
     .pipe(istanbul.writeReports());
 });
@@ -60,7 +49,3 @@ gulp.task('coverage', ['test'], () => {
     .pipe(coveralls());
 });
 
-
-gulp.task('default', ['coverage'], () => {
-
-});
